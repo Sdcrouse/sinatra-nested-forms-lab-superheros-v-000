b96cd968da024e4197dfe7bc9fbccd9b78e9cfcd
Steven Crouse <sdcrouse91@gmail.com>
2019-07-11 07:36:46 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,9 @@ class App < Sinatra::Base
     
     post '/teams' do
       @team = Team.new(params["team"])
+      
       params["team"]["members"].each { |member| Hero.new(member) }
+      
       @heroes = Hero.all
       
       erb :team
